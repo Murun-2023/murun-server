1f1b986f57ef1023fd1ec50097b348d4c15294c5
minseokkang <aaaa727@khu.ac.kr>
2023-03-19 01:44:38 +0900

    feat:음악 가져ㅑì반환까지  성공

--- src/main/kotlin/com/example/murun/controller/MusicController.kt
+++ src/main/kotlin/com/example/murun/controller/MusicController.kt
@@ -1,15 +1,26 @@
 package com.example.murun.controller
 
-import org.springframework.stereotype.Controller
+import com.example.murun.dataclass.BpmRequestDto
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class MusicController {
+@RestController
+class MusicController (private val resourceLoader: ResourceLoader){
 
     @GetMapping("/test")
     @ResponseBody
-    fun findMusic(): String{
-        return "hello";
+    fun findMusic(@RequestBody requestDto: BpmRequestDto): ResponseEntity<Resource>{
+        println("test :${requestDto.bpm}")
+        val fileResource = resourceLoader.getResource("classpath:/static/test.mp3")
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"test.mp3\"")
+                .body(fileResource)
     }
+
 }
---
+++ src/main/kotlin/com/example/murun/dataclass/BpmRequestDto.kt
@@ -0,0 +1,5 @@
+package com.example.murun.dataclass
+
+data class BpmRequestDto(
+        val bpm: Int
+)
