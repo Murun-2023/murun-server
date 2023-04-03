346473d53c4ded8c8b057eaab03724efa8545489
kms <aaaa727@khu.ac.kr>
2023-04-03 22:37:12 +0900

    feat: 메타정보 우선으로 로직 변경.

--- src/main/kotlin/com/example/murun/domain/SongService.kt
+++ src/main/kotlin/com/example/murun/domain/SongService.kt
@@ -9,9 +9,13 @@ import org.jaudiotagger.tag.FieldKey
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.multipart.commons.CommonsMultipartFile
+import java.io.ByteArrayInputStream
 import java.io.File
+import java.lang.Exception
 import java.lang.RuntimeException
 import java.util.*
+import javax.imageio.ImageIO
 
 @Service
 class SongService(
@@ -27,43 +31,79 @@ class SongService(
     }
 
     fun addSong(title: String, artist: String, bpm: Int, albumImage: MultipartFile, song: MultipartFile): SongResponseDto {
-        //albumImage 저장
-        val albumImageUrl = uploadAlbumImage(albumImage, title, bpm)
-        //song 파일 저장
-        val songUrl = uploadSong(song, title, bpm)
-        //추후 추가여부를 위해 남겨놓음 요건이 변경될 가능성이 있음.
-        /*
-        val file = File(multipartFile.originalFilename)
-        file.writeBytes(multipartFile.bytes)
+        var tmpTitle: String = title
+        var tmpArtist: String = artist
+        var tmpBpm: Int  = bpm
+        var tmpAlbumImage: File
+
+        val file = File(song.originalFilename)
+        var imageFile = File(albumImage.originalFilename)
+
+        file.writeBytes(song.bytes)
+        imageFile.writeBytes(albumImage.bytes)
+
+        tmpAlbumImage = imageFile
 
         val mp3: MP3File = AudioFileIO.read(file) as MP3File
         file.delete()
 
-        val title: String = mp3.tag.getFirst(FieldKey.TITLE)
-        val bpm: String = mp3.tag.getFirst(FieldKey.BPM)
+        try{
+            println("mp3 artist: ${mp3.tag.getFirst(FieldKey.ARTIST)}")
+            tmpArtist = if(mp3.tag.getFirst(FieldKey.ARTIST) == "") artist else mp3.tag.getFirst(FieldKey.ARTIST)
+        } catch (e: Exception){
+            e.stackTrace
+        }
+
+        try{
+            tmpTitle= if(mp3.tag.getFirst(FieldKey.TITLE) == "") title else mp3.tag.getFirst(FieldKey.TITLE)
+        }catch (e: Exception){
+            e.stackTrace
+        }
+        try{
+            tmpBpm = mp3.tag.getFirst(FieldKey.BPM).toInt()
+        } catch (e: Exception){
+            e.stackTrace
+        }
+        try {
+            val imageData = mp3.tag.firstArtwork.binaryData
+            val image = ImageIO.read(ByteArrayInputStream(imageData))
+            imageFile = File("${tmpTitle}.png")
+            ImageIO.write(image, "png", imageFile)
+            tmpAlbumImage = imageFile
+        } catch (e: Exception){
+            e.stackTrace
+        }
         val uuid: String = UUID.randomUUID().toString()
-        val url = s3UploaderService.uploadBpmFile(multipartFile, title, bpm)
-         */
-        val uuid: String = UUID.randomUUID().toString()
-        songJpaRepository.saveBpmSong(title, artist, albumImageUrl, bpm, uuid, songUrl)
-        return SongResponseDto(uuid, title, artist, albumImageUrl, songUrl)
+
+        //추출 값
+        println("======---title:${tmpTitle}, artist:${tmpArtist} bpm:${tmpBpm}, image: ${tmpAlbumImage}")
+
+        //albumImage 저장
+        val albumImageUrl = uploadAlbumImage(tmpAlbumImage, tmpTitle, tmpBpm)
+        //song 파일 저장
+        val songUrl = uploadSong(song, tmpTitle, tmpBpm)
+        tmpAlbumImage.delete()
+
+        songJpaRepository.saveBpmSong(tmpTitle, tmpArtist, albumImageUrl, tmpBpm, uuid, songUrl)
+        return SongResponseDto(uuid, tmpTitle, tmpArtist, albumImageUrl, songUrl)
     }
 
-    fun getCorrectUUIDSong(uuid: String): SongResponseDto{
+    fun getCorrectUUIDSong(uuid: String): SongResponseDto {
         val song = songJpaRepository.getCorrectUUIDSong(uuid)
-        song?.let{
+        song?.let {
             return convertDto(song)
         }
         throw RuntimeException("해당 uuid로 조회가 불가능합니다.")
     }
 
-    private fun uploadAlbumImage(albumImage: MultipartFile, title: String, bpm: Int): String {
+    private fun uploadAlbumImage(albumImage: File, title: String, bpm: Int): String {
         return s3UploaderService.uploadAlbumImage(albumImage, title, bpm)
     }
 
     private fun uploadSong(song: MultipartFile, title: String, bpm: Int): String {
         return s3UploaderService.uploadSongFile(song, title, bpm)
     }
+
 
     private fun convertDto(song: SongEntity): SongResponseDto {
         return SongResponseDto(song.uuid, song.title, song.artist, song.albumImage, song.downloadUrl)
--- src/main/kotlin/com/example/murun/infrastructure/aws/S3UploaderService.kt
+++ src/main/kotlin/com/example/murun/infrastructure/aws/S3UploaderService.kt
@@ -1,9 +1,10 @@
 package com.example.murun.infrastructure.aws
 
 import org.springframework.web.multipart.MultipartFile
+import java.io.File
 
 interface S3UploaderService {
     fun getBpmFile(bpm: Int)
-    fun uploadAlbumImage(file: MultipartFile, title: String, bpm: Int): String
+    fun uploadAlbumImage(file: File, title: String, bpm: Int): String
     fun uploadSongFile(file: MultipartFile, title: String, bpm: Int): String
 }
